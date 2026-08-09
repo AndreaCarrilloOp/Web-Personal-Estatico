66cf942b00f4dd5fa6addd24f662957f0dea8af8
Valky <acarrillo.vlk@gmail.com>
2026-08-09 00:04:57 -0600

    feat: agregar componente de encabezado

---
+++ TheHeader.vue
@@ -0,0 +1,14 @@
+<!-- app/components/TheHeader.vue -->
+<template>
+  <header>
+    <h2>Andrea Carrillo</h2>
+    <nav>
+      <NuxtLink to="/">Inicio</NuxtLink> |
+      <NuxtLink to="/sobre-mi">Sobre mí</NuxtLink> |
+      <NuxtLink to="/proyectos">Proyectos</NuxtLink> |
+      <NuxtLink to="/contacto">Contacto</NuxtLink> |
+      <NuxtLink to="/blog">Blog</NuxtLink>
+    </nav>
+    <hr>
+  </header>
+</template>
